08dbdc428e2e7c170b7924841606b8e96268c263
Shivendra Pratap Singh <shivendra2396@gmail.com>
2019-03-01 18:31:29 +0530

    Add files via upload

---
+++ ToastMessage/ToastMessage/ToastMessage/PageHome.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToastMessage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageHome : ContentPage
+	{
+		public PageHome ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            ButtonToast.Clicked += ButtonToast_Clicked;
+        }
+
+        private void ButtonToast_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<Toast>().Show("I am shivendra");
+        }
+    }
+}
