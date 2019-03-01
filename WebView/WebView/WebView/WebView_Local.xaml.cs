ab72b26c48fec50f9458d36ebdb1c56c9af058f7
Shivendra Pratap Singh <shivendra2396@gmail.com>
2019-03-01 18:25:22 +0530

    Add files via upload

---
+++ WebView/WebView/WebView/WebView_Local.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WebView
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WebView_Local : ContentPage
+	{
+		public WebView_Local ()
+		{
+			InitializeComponent ();
+
+            Webview.Source = new HtmlWebViewSource
+            {
+                Html = @"<html><body>An html string</body></html>"
+            };
+        }
+
+        
+
+       
+    }
+}
